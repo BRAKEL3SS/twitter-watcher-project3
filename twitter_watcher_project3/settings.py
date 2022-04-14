1855ad8c9fab81ab546aadf623dc916732507ea7
BRAKEL3SS <rck496@yahoo.com>
2022-04-14 14:20:47 -0700

    Moves secret to .env

--- twitter_watcher_project3/settings.py
+++ twitter_watcher_project3/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jtk=5iztdjyr@@3s)zo$s#x5*n2#b&+m^t)ef5^391u#af&myz'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
