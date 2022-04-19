af3d14a46c5ef996842b78291726033c3c4ddfb8
keanii2018 <keani.mpcc@outlook.com>
2022-04-19 11:51:12 -0700

    add tweet model and relation to comment

--- main_app/models.py
+++ main_app/models.py
@@ -5,10 +5,20 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Tweet(models.Model):
+    tweetId = models.CharField(max_length=100)
+    text = models.TextField(max_length=300)
+    class Meta:
+        unique_together = [('tweetId', 'text')]
+    def __str__(self):
+        return self.text
+        
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=250)
     date = models.DateField('comment date', default=date.today)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.text
     
